2e9a85cc52e22e6e84e5686aa7b8b20b690ec1c1
Raafat Ashraf <raafat.ashraf.17@gmail.com>
2024-10-06 20:59:20 +0300

    Add validation to update leave type

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/Update/UpdateLeaveTypeCommand.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/Update/UpdateLeaveTypeCommand.cs
@@ -1,6 +1,6 @@
 namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.Update;
 
-public class UpdateLeaveTypeCommand : IRequest<Unit>
+public abstract class UpdateLeaveTypeCommand : IRequest<Unit>
 {
     public string Name { get; set; } = string.Empty;
     public int DefaultDays { get; set; }
