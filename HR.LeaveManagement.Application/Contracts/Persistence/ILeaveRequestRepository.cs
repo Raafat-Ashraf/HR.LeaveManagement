f737974b10111ba100e1b5d8d79b605f3dddd66b
Raafat Ashraf <raafat.ashraf.17@gmail.com>
2024-10-06 23:23:18 +0300

    Add 3 method to leave request repository

--- HR.LeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -4,5 +4,7 @@ namespace HR.LeaveManagement.Application.Contracts.Persistence;
 
 public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
 {
-
+    Task<LeaveRequest?> GetLeaveRequestWithDetailsAsync(int id);
+    Task<List<LeaveRequest>> GetLeaveRequestsWithDetails();
+    Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId);
 }
--- HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -1,10 +1,39 @@
 using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Domain;
 using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace HR.LeaveManagement.Persistence.Repositories;
 
-public class LeaveRequestRepository(HrDatabaseContext hrDatabaseContext) : GenericRepository<LeaveRequest>(hrDatabaseContext), ILeaveRequestRepository
+public class LeaveRequestRepository(HrDatabaseContext hrDatabaseContext)
+    : GenericRepository<LeaveRequest>(hrDatabaseContext), ILeaveRequestRepository
 {
 
+    public async Task<LeaveRequest?> GetLeaveRequestWithDetailsAsync(int id)
+    {
+        var leaveRequest = await _hrDatabaseContext.LeaveRequests
+            .Include(q => q.LeaveType)
+            .FirstOrDefaultAsync(q => q.Id == id);
+
+        return leaveRequest;
+    }
+
+    public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+    {
+        var leaveRequests = await _hrDatabaseContext.LeaveRequests
+            .Include(x => x.LeaveType)
+            .ToListAsync();
+
+        return leaveRequests;
+    }
+
+    public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId)
+    {
+        var leaveRequests = await _hrDatabaseContext.LeaveRequests
+            .Where(x=>x.RequestingEmployeeId == userId)
+            .Include(x=>x.LeaveType)
+            .ToListAsync();
+
+        return leaveRequests;
+    }
 }
